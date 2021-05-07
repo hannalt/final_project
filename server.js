1b6d1510113d16eacb19be3599e03df209aa06ab
Hanna L Tischer <hannalt@live.unc.edu>
2021-05-07 16:13:24 -0400

    work please

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
+const routes = require('./routes/api');
+const { MongoClient } = require('mongodb');
 
 const http = require('http');
 
@@ -11,9 +13,9 @@ const PORT = process.env.PORT || 8000;
 //mongodb+srv://hanna:<kcb8cXbzGAEzdGlJ>@useraccounts.bx3zc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 //heroku => comp426finalhannalt
 
+const url = "mongodb+srv://hanna:kcb8cXbzGAEzdGlJ@useraccounts.bx3zc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-const routes = require('./routes/api');
-const { MongoClient } = require('mongodb');
 //const sockets = require('./routes/sockets');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
@@ -31,6 +33,19 @@ app.use(express.urlencoded({ extended: false }))
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('./client/build'))
 }
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.use(morgan('tiny'));
 app.use('/api', routes);
